79f3e7692136c5e22e18248055bacbbeee4719df
Nathan Dockens <ndockens92@gmail.com>
2023-10-06 15:58:02 -0500

    Add initial planet objects

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,31 @@ import { Pane } from 'tweakpane';
 const pane = new Pane();
 const scene = new THREE.Scene();
 
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 'yellow' });
+const sun = new THREE.Mesh(sphereGeometry, sunMaterial);
+sun.scale.setScalar(5);
+scene.add(sun);
+
+const earthMaterial = new THREE.MeshBasicMaterial({ color: 'blue' });
+const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
+earth.position.x = 10;
+scene.add(earth);
+
+const moonMaterial = new THREE.MeshBasicMaterial({ color: 'grey' });
+const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
+moon.scale.setScalar(0.3);
+moon.position.x = 2;
+earth.add(moon);
+
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
   0.1,
   10000
 );
+camera.position.x = 50;
 
 const canvas = document.querySelector('canvas.threejs') as HTMLCanvasElement;
 
